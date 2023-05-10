b0fac6df47045e2023cc9f08d24ccf5bf3aa9b47
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-10 11:02:14 +0200

    Create button to reveal project creation form

--- src/projects-page.js
+++ src/projects-page.js
@@ -3,13 +3,17 @@ import addProject from './add-project';
 
 const contentDiv = document.querySelector('.content');
 const projectsDiv = document.createElement('div');
+const addProjectBtn = document.createElement('button');
 
+addProjectBtn.classList.add('add-project-btn');
+addProjectBtn.textContent = '+ Add Project'
 projectsDiv.classList.add('projects')
+addProjectBtn.addEventListener('click', projectForm)
+projectNameBtn.addEventListener('click', addProject);
 
 function projectsPage() {
-  projectForm();
-  projectNameBtn.addEventListener('click', addProject);
   contentDiv.appendChild(projectsDiv);
+  projectsDiv.appendChild(addProjectBtn);
 }
 
 export default projectsPage;
