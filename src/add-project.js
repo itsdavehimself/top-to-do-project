31e20b7cd915303e7a33df65f5046717d91631d7
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-10 12:52:21 +0200

    Add delete project button
    Button deletes project element from DOM and from projectArr

--- src/add-project.js
+++ src/add-project.js
@@ -1,18 +1,34 @@
-import {createProject} from "./task";
+import { createProject, projectArr } from "./task";
 import showProject from "./project-view";
 
 function displayProject(name, id) {
   const projectsDiv = document.querySelector('.projects');
+  const projectContainer = document.createElement('div');
   const projectCard = document.createElement('div');
+  const projectDeleteBtn = document.createElement('button');
+
+  projectContainer.classList.add('project-container');
+  projectContainer.setAttribute('id', id);
+  projectDeleteBtn.classList.add('project-delete-btn');
+  projectDeleteBtn.textContent = 'Delete project';
   projectCard.classList.add('project-card');
   projectCard.setAttribute('id', id);
   projectCard.textContent = `${name}`;
-  projectsDiv.appendChild(projectCard);
+  projectsDiv.appendChild(projectContainer);
+  projectContainer.appendChild(projectCard);
+  projectContainer.appendChild(projectDeleteBtn);
+
   projectsDiv.addEventListener('click', (e) => {
     if (e.target.className === 'project-card') {
       showProject(e.target.textContent, e.target.id);
     }
-  })
+  });
+
+  projectDeleteBtn.addEventListener('click', () => {
+    projectsDiv.removeChild(projectContainer);
+    const projectIndex = projectArr.findIndex(object => object.id === Number(id))
+    projectArr.splice(projectIndex, 1);
+  }); 
 }
 
 function addProject(e) {
@@ -20,6 +36,7 @@ function addProject(e) {
   const project = createProject(projectName);
   const addProjectForm = document.querySelector('.create-project');
   const contentDiv = document.querySelector('.content');
+
   displayProject(projectName, project.id);
   console.log(project);
   console.log(project.id);
