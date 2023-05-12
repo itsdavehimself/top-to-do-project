0fdfcf3a7827ddb30062b3d9f12bee69d664a221
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-12 13:51:55 +0200

    Add div for all tasks on home page

--- src/home-page.js
+++ src/home-page.js
@@ -1,10 +1,15 @@
+import { projectArr } from "./task";
+
 const contentDiv = document.querySelector('.content');
 const homeDiv = document.createElement('div');
+const allTasks = document.createElement('div');
 
 homeDiv.classList.add('home-container');
+allTasks.classList.add('all-tasks');
 
 function homePage() {
   contentDiv.appendChild(homeDiv);
+  homeDiv.appendChild(allTasks);
 }
 
 export default homePage;
