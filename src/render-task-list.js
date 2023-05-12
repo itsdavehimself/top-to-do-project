067fa8fa808ef3f33ca5e43eb76991e88573e50b
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-12 12:47:08 +0200

    Create task card render module
    Renders task cards for existing tasks on the show project page

--- src/render-task-list.js
+++ src/render-task-list.js
@@ -1,8 +1,27 @@
 function renderTaskList(project) {
   const tasksDiv = document.querySelector('.tasks');
   const tasks = project.taskArr
+  tasksDiv.replaceChildren();
   for (let i = 0; i < tasks.length; i+=1) {
-    console.log(tasks[i].taskName);
+    const taskCard = document.createElement('div');
+    const taskCardTitle = document.createElement('p');
+    const taskCardDescription = document.createElement('p');
+    const taskCardDate = document.createElement('p');
+    const taskCardPriority = document.createElement('p');
+    taskCard.classList.add('task-card');
+    taskCardTitle.classList.add('task-card-title');
+    taskCardTitle.textContent = project.taskArr[i].taskName;
+    taskCardDescription.classList.add('task-card-description')
+    taskCardDescription.textContent = project.taskArr[i].description;
+    taskCardDate.classList.add('task-card-date');
+    taskCardDate.textContent = project.taskArr[i].dueDate;
+    taskCardPriority.classList.add('task-card-priority');
+    taskCardPriority.textContent = project.taskArr[i].priority;
+    tasksDiv.appendChild(taskCard);
+    taskCard.appendChild(taskCardTitle);
+    taskCard.appendChild(taskCardDescription);
+    taskCard.appendChild(taskCardDate);
+    taskCard.appendChild(taskCardPriority);
   }
 }
 
