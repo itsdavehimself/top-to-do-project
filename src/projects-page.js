1986b2fbb4043e9a2dbc21bfcdab692c9f703fa4
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-13 10:25:54 +0200

    Restructure DOM elements and class names

--- src/projects-page.js
+++ src/projects-page.js
@@ -3,21 +3,24 @@ import renderProjectList from "./render-project-list";
 
 const contentDiv = document.querySelector('.content');
 const projectsPageDiv = document.createElement('div');
-const addProjectDiv = document.createElement('div');
+const projectsPageTitle = document.createElement('p');
+const projectsPageHeader = document.createElement('div');
 const addProjectBtn = document.createElement('button');
 const currentProjectsDiv = document.createElement('div');
 currentProjectsDiv.classList.add('current-projects');
 
 projectsPageDiv.classList.add('projects');
-addProjectDiv.classList.add('add-project');
+projectsPageHeader.classList.add('projects-header');
 addProjectBtn.classList.add('add-project-btn');
 addProjectBtn.textContent = '+ Add Project';
 addProjectBtn.addEventListener('click', projectForm);
+projectsPageTitle.textContent = 'Projects';
 
 function projectsPage() {
   contentDiv.appendChild(projectsPageDiv);
-  projectsPageDiv.appendChild(addProjectDiv);
-  addProjectDiv.appendChild(addProjectBtn);
+  projectsPageDiv.appendChild(projectsPageHeader);
+  projectsPageHeader.appendChild(projectsPageTitle);
+  projectsPageHeader.appendChild(addProjectBtn);
   projectsPageDiv.appendChild(currentProjectsDiv);
   renderProjectList();
 }
