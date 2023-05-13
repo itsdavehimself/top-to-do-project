205fed4121a6626df28c50f08707a7b7221722c4
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-13 10:47:55 +0200

    Add horizontal line

--- src/projects-page.js
+++ src/projects-page.js
@@ -7,6 +7,7 @@ const projectsPageTitle = document.createElement('p');
 const projectsPageHeader = document.createElement('div');
 const addProjectBtn = document.createElement('button');
 const currentProjectsDiv = document.createElement('div');
+const breakLine = document.createElement('hr');
 currentProjectsDiv.classList.add('current-projects');
 
 projectsPageDiv.classList.add('projects');
@@ -15,12 +16,14 @@ addProjectBtn.classList.add('add-project-btn');
 addProjectBtn.textContent = '+ Add Project';
 addProjectBtn.addEventListener('click', projectForm);
 projectsPageTitle.textContent = 'Projects';
+breakLine.classList.add('break');
 
 function projectsPage() {
   contentDiv.appendChild(projectsPageDiv);
   projectsPageDiv.appendChild(projectsPageHeader);
   projectsPageHeader.appendChild(projectsPageTitle);
   projectsPageHeader.appendChild(addProjectBtn);
+  projectsPageDiv.appendChild(breakLine);
   projectsPageDiv.appendChild(currentProjectsDiv);
   renderProjectList();
 }
