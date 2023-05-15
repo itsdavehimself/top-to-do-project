fe88206c8331bfe19539e8cb886047b0695e2f04
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-15 12:26:35 +0200

    Create modal for project creation form

--- src/add-project-form.js
+++ src/add-project-form.js
@@ -1,38 +1,66 @@
 import { createProject } from './task'
 import renderProjectList from './render-project-list';
 
+const coveringPanel = document.createElement('div');
+const projectCreateFormDiv = document.createElement('div');
+const createFormTitle = document.createElement('p');
 const projectCreateForm = document.createElement('form');
 const projectNameLabel = document.createElement('label');
 const projectNameInput = document.createElement('input');
+const projectCreateBtnDiv = document.createElement('div');
 const projectNameBtn = document.createElement('button');
-projectCreateForm.classList.add('create-project')
-projectNameLabel.textContent = "Name";
+const cancelProjectBtn = document.createElement('button');
+coveringPanel.classList.add('covering-panel');
+projectCreateFormDiv.classList.add('create-project-div');
+createFormTitle.classList.add('create-form-title');
+projectCreateForm.classList.add('create-project');
+projectCreateBtnDiv.classList.add('project-form-btn-div');
+createFormTitle.textContent = 'Add project';
+projectNameLabel.textContent = 'Name';
 projectNameLabel.setAttribute('for', 'project-name');
 projectNameInput.setAttribute('id', 'project-name');
 projectNameInput.setAttribute('name', 'project-name');
 projectNameInput.setAttribute('type', 'text');
 projectNameInput.setAttribute('autocomplete', 'off');
-projectNameBtn.textContent = "Add";
+projectNameBtn.textContent = 'Add';
 projectNameBtn.classList.add('add-project-btn-form');
+cancelProjectBtn.classList.add('cancel-project-btn-form');
+cancelProjectBtn.textContent = 'Cancel';
 
 function addProject(e) {
-  const projectsDiv = document.querySelector('.projects');
+  const mainDiv = document.querySelector('.main');
   const projectName = document.getElementById('project-name').value;
   const newProject = createProject(projectName);
   console.log(newProject);
   document.getElementById('project-name').value = '';
-  projectsDiv.removeChild(projectCreateForm);
+  mainDiv.removeChild(coveringPanel);
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function cancelProjectAdd(e) {
+  const mainDiv = document.querySelector('.main');
+  document.getElementById('project-name').value = '';
+  mainDiv.removeChild(coveringPanel);
   e.preventDefault();
 }
 
 projectNameBtn.addEventListener('click', addProject);
 projectNameBtn.addEventListener('click', renderProjectList);
+cancelProjectBtn.addEventListener('click', cancelProjectAdd);
+projectCreateFormDiv.addEventListener('click', (e) => e.stopPropagation())
+coveringPanel.addEventListener('click', cancelProjectAdd);
 
 function projectForm() {
-  const projectsDiv = document.querySelector('.projects');
-  projectsDiv.appendChild(projectCreateForm);
+  const mainDiv = document.querySelector('.main');
+  mainDiv.appendChild(coveringPanel);
+  coveringPanel.appendChild(projectCreateFormDiv);
+  projectCreateFormDiv.appendChild(createFormTitle);
+  projectCreateFormDiv.appendChild(projectCreateForm);
   projectCreateForm.appendChild(projectNameLabel);
   projectCreateForm.appendChild(projectNameInput);
+  projectCreateForm.appendChild(projectCreateBtnDiv);
+  projectCreateBtnDiv.appendChild(cancelProjectBtn);
   projectCreateForm.appendChild(projectNameBtn);
   projectNameInput.focus();
 };
