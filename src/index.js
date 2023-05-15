08a99b8a3b07bc77ea2cf6edf10ce56cf8cc385a
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-15 14:09:48 +0200

    Add active class

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import "./style.css";
 import homePage from "./home-page";
 import projectsPage from "./projects-page";
-import { sideBar, homeBtn, projectsPageBtn } from "./sidebar";
+import { sideBar, homeBtnDiv, projectsBtnDiv } from "./sidebar";
 
 const contentDiv = document.querySelector('.content');
 
 function openHomePage() {
   contentDiv.removeChild(contentDiv.firstChild);
+  homeBtnDiv.classList.add('active');
+  projectsBtnDiv.classList.remove('active');
   homePage();
 }
 
 function openProjectsPage() {
   contentDiv.removeChild(contentDiv.firstChild);
+  projectsBtnDiv.classList.add('active');
+  homeBtnDiv.classList.remove('active');
   projectsPage();
 }
 
 
-homeBtn.addEventListener('click', openHomePage);
-projectsPageBtn.addEventListener('click', openProjectsPage);
+homeBtnDiv.addEventListener('click', openHomePage);
+projectsBtnDiv.addEventListener('click', openProjectsPage);
+homeBtnDiv.classList.add('active');
 
 homePage();
 sideBar();
