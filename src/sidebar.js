cbdeb8214a5ebb3b1e31b698f905ec4edbec9966
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-15 14:10:05 +0200

    Creat divs for sidebar button formatting

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,15 +1,35 @@
-const sideBarDiv = document.querySelector('.sidebar');
-const projectsPageBtn = document.createElement('button');
-const homeBtn = document.createElement('button');
+import homeIcon from './home-outline.svg';
+import listIcon from './format-list-bulleted.svg';
 
+const sideBarDiv = document.querySelector('.sidebar');
+const sideBarBtnDiv = document.createElement('div');
+const projectsBtnDiv = document.createElement('div');
+const projectsPageBtn = document.createElement('div');
+const homeBtnDiv = document.createElement('div');
+const homeBtn = document.createElement('div');
+const homeImg = document.createElement('img');
+const listImg = document.createElement('img');
+
+homeImg.src = homeIcon;
+homeImg.classList.add('home-icon');
+listImg.src = listIcon;
+listImg.classList.add('list-icon');
+sideBarBtnDiv.classList.add('sidebar-btn-div');
+projectsBtnDiv.classList.add('projects-btn-div');
 projectsPageBtn.classList.add('projects-page-btn');
 projectsPageBtn.textContent = 'Projects';
+homeBtnDiv.classList.add('home-btn-div');
 homeBtn.classList.add('home-btn');
 homeBtn.textContent = 'Home';
 
 function sideBar() {
-  sideBarDiv.appendChild(homeBtn);
-  sideBarDiv.appendChild(projectsPageBtn);
+  sideBarDiv.appendChild(sideBarBtnDiv);
+  sideBarBtnDiv.appendChild(homeBtnDiv);
+  homeBtnDiv.appendChild(homeImg);
+  homeBtnDiv.appendChild(homeBtn);
+  sideBarBtnDiv.appendChild(projectsBtnDiv);
+  projectsBtnDiv.appendChild(listImg);
+  projectsBtnDiv.appendChild(projectsPageBtn);
 }
 
-export { sideBar, homeBtn, projectsPageBtn };
+export { sideBar, homeBtnDiv, projectsBtnDiv };
