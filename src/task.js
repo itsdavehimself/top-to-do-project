b90f98313b5f26b85898c3e57e17b4127b6f996f
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-17 07:47:21 +0200

    Add local storage for project objects

--- src/task.js
+++ src/task.js
@@ -1,5 +1,7 @@
-const projectArr = localStorage.getItem('array') ?
-JSON.parse(localStorage.getItem('array')) : [] ;
+const storageArr = localStorage.getItem('array') ?
+JSON.parse(localStorage.getItem('array')) : [];
+
+const projectArr = [];
 
 function createProject (name) {
   const project = {
@@ -29,12 +31,14 @@ function createProject (name) {
   return project
 };
 
-const myFirstProject = createProject('My first project');
-myFirstProject.createTask('My first task', 'This is my first task, ever!', '2023-08-31', 'low', '0');
-myFirstProject.createTask('Overdue task', 'This task is way overdue.', '2023-01-05', 'medium', '1');
-const secondProject = createProject('Yardwork');
-secondProject.createTask('Mow lawn', 'Remember to charge batteries.', '2023-05-17', 'high', '2');
-const thirdProject = createProject('Spring cleaning');
-thirdProject.createTask('Clean out garage drawers', 'Just toss it!', '2023-05-28', 'low', '3');
+for (let i = 0; i < storageArr.length; i+=1) {
+  console.log(storageArr[i])
+  const newObj = Object.assign(createProject(), storageArr[i]);
+  console.log('new obj...');
+  console.log(newObj)
+  console.log('projectarr:');
+  console.log(projectArr);
+  localStorage.setItem('array', JSON.stringify(projectArr))
+}
 
 export { createProject, projectArr };
